698f59eb9bac5314e8b47f82091084a3154f37fd
VladSvichar <vlad.svichar@starioncasinos.com>
2018-09-03 23:14:30 -0700

    fixes

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
-<<<<<<< HEAD
 
 const port = 3000;
 const app = express();
@@ -17,39 +16,3 @@ app.listen(port, function(err) {
         open('http://localhost:' + port);
     }
 });
-=======
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
-
-/* eslint-disable no-console */
-
-const port = 3000;
-const app = express();
-const compiler = webpack(config);
-
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
-});
-
-app.get('/users', function(req, res) {
-  // Hard coding for simplicity. Pretend this hits a real database
-  res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
-  ]);
-});
-
-app.listen(port, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    open('http://localhost:' + port);
-  }
-});
->>>>>>> 27a1f6549da23880f2275685d970d06b66cb22e5
